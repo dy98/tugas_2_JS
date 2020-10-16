f15bf98cf365915aea824011edbc691ff1341217
Dwi Yulianto <98dwiyulianto@gmail.com>
2020-10-17 05:36:32 +0700

    Tugas 2 JS
    tugas untuk Niomic

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+let umur = 10;
+var nama = 'Ujang';
+let lulus = true;
+const kelas = null;
+const alamat = ['Jakarta', 'Jl.Haji Yamin', ['Ciledug', 22]];
+
+console.log('Nama : ' + nama + '\n' + 'Umur : ' + umur);
+
+if (lulus) {
+    console.log('status : lulus');
+}
+
+console.log('Alamat : ' + alamat[0] + ', ' + alamat[1] + '\nkecamatan : ' + alamat[2][0] + '\nNomor : ' + alamat[2][1]);
